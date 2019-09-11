db2999ad03a900c1d37d32af2964620a68de8439
Hércules Lima <hl3058317@gmail.com>
2019-09-11 14:02:35 -0300

    Criando Próprios Componentes

--- react_native_basico/PrimeiroProjeto/App.js
+++ react_native_basico/PrimeiroProjeto/App.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 import { View, Text, Image } from 'react-native';
 
+class Imagem extends Component{
+  render(){
+    let imagem = {uri:'https://www.google.com.br/logos/'+this.props.nome+'.jpg'}
+
+    return (
+
+      <Image source={imagem} style={{width:parseInt(this.props.largura),height:parseInt(this.props.altura)}} />
+
+
+      )
+
+
+  }
+
+
+}
+
+
 export default class PrimeiroProjeto extends Component{
   render(){
-    let imagem = {uri:'https://cdn.theatlantic.com/assets/media/img/mt/2018/11/shutterstock_552503470/lead_720_405.jpg?mod=1541605820'}
+    let imagem = {uri:'https://www.google.com.br/logos/google.jpg'}
     return (
       <View>
         <Text>Olá Mundo!</Text>
         <Text style={{fontSize:25,color:'red', margin:20}}>Olá Mundo!</Text>
-        <Image source={imagem} style={{width:300,height:300,margin:20}} />
+
+        <Imagem nome='google' largura='700' altura='200' />
+
+        
       </View>
     );
   }
