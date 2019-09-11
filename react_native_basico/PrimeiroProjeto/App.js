fe2d1b54c6e6e5bfb2d87c0116d352fd5240982d
Hércules Lima <hl3058317@gmail.com>
2019-09-11 14:24:15 -0300

    Tamanho Fixo e Dinâmico

--- react_native_basico/PrimeiroProjeto/App.js
+++ react_native_basico/PrimeiroProjeto/App.js
@@ -4,32 +4,11 @@ import { View, Text,   StyleSheet } from 'react-native';
 export default class PrimeiroProjeto extends Component{
   render(){
     return (
-      <View style={styles.padrao}>
-          <Text style={styles.azulGrande}>Texto 1</Text>
-          <Text style={styles.vermelho}>Texto 2</Text>
-          <Text style={[styles.azulGrande,styles.vermelho]}>Texto 3</Text>
-          <Text style={[styles.vermelho,styles.azulGrande]}>Texto 4</Text>
+      <View style={{flex:1, backgroundColor:'black'}}>
+      <View style={{height:70,  backgroundColor:'blue'}}></View>
+      <View style={{flex:1,  backgroundColor:'green'}}></View>
+      <View style={{height:50,  backgroundColor:'yellow'}}></View>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  padrao: {
-    paddingTop:20,
-    backgroundColor:'#00FF00'
-  },
-
-  azulGrande: {
-    color:'#0000FF',
-    fontSize:30,
-    textAlign:'center'
-  },
-
-  vermelho: {
-    color:'#FF0000',
-    fontSize:15
-  }
-
-
-});
