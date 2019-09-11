b50bc6ec005183a34b7410212559488915836736
Hércules Lima <hl3058317@gmail.com>
2019-09-11 13:55:19 -0300

    Detalhes sobre ES6

--- react_native_basico/PrimeiroProjeto/App.js
+++ react_native_basico/PrimeiroProjeto/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
-export default class PrimeiroProjeto extends Component{
+export default class PrimeiroRNProjeto extends Component{
+
+  somar(n1,n2){
+    return n1 + n2;
+  }
+
   render(){
+
     return (
       <View>
-        <Text>Olá Mundo!</Text>
+
+        <Text style={styles.texto}>A soma de 10 + 10 é { this.somar(10,10)}</Text>
+
+        <Button title= "Aperte" onPress={() => alert("Fui Apertado!")} ></Button>
+
       </View>
     );
   }
+
 }
+
+const styles = StyleSheet.create({
+  texto:{
+    fontSize:30
+  }
+});
