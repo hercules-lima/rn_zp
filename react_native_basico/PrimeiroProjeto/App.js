51c27e904dd240d8d0c285383e20a3100b75ce04
Hércules Lima <hl3058317@gmail.com>
2019-09-11 13:52:30 -0300

    Estrutura Básica de um App

---
+++ react_native_basico/PrimeiroProjeto/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class PrimeiroProjeto extends Component{
+  render(){
+    return (
+      <View>
+        <Text>Olá Mundo!</Text>
+      </View>
+    );
+  }
+}
